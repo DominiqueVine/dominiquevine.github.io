2fe12296571d17a66ee2c61482e7eb8264287c0b
DominiqueVine <dcvin1@student.monash.edu>
2017-05-18 10:41:05 +1000

    type lessons

--- typelessons/assets/js/main.js
+++ typelessons/assets/js/main.js
@@ -20,6 +20,56 @@ $(document).ready(function() {
 
 
 
+ window.onscroll = function() {scrollFunction()};
+
+ function scrollFunction() {
+     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+         document.getElementById("myBtn").style.display = "block";
+     } else {
+         document.getElementById("myBtn").style.display = "none";
+     }
+ }
+
+ // When the user clicks on the button, scroll to the top of the document
+ function topFunction() {
+     document.body.scrollTop = 0; // For Chrome, Safari and Opera
+     document.documentElement.scrollTop = 0; // For IE and Firefox
+ }
+
+
+
+
+ $(window).on("load",function() {
+   $(window).scroll(function() {
+     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+     $(".fade").each(function() {
+       /* Check the location of each desired element */
+       var objectBottom = $(this).offset().top + $(this).outerHeight();
+
+       /* If the element is completely within bounds of the window, fade it in */
+       if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+         if ($(this).css("opacity")==0) {$(this).fadeTo(700,1);}
+       } else { //object goes out of view (scrolling up)
+         if ($(this).css("opacity")==1) {$(this).fadeTo(700,0);}
+       }
+     });
+   }).scroll(); //invoke scroll-handler on page-load
+ });
+
+
+
+ $('.beatriceblurb').mouseenter(function() {
+ $('.beatriceblurb__image').addClass('showme');
+ });
+
+ $('.beatriceblurb').mouseleave(function() {
+ $('.beatriceblurb__image').removeClass('showme');
+ });
+
+
+
+
+
 
 
 
