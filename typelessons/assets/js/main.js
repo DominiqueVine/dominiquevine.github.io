f9a27e0e67799d84bcb238349783399e6319912e
DominiqueVine <dcvin1@student.monash.edu>
2017-05-21 21:30:51 +1000

    type lessons

--- typelessons/assets/js/main.js
+++ typelessons/assets/js/main.js
@@ -20,23 +20,6 @@ $(document).ready(function() {
 
 
 
- window.onscroll = function() {scrollFunction()};
-
- function scrollFunction() {
-     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-         document.getElementById("myBtn").style.display = "block";
-     } else {
-         document.getElementById("myBtn").style.display = "none";
-     }
- }
-
- // When the user clicks on the button, scroll to the top of the document
- function topFunction() {
-     document.body.scrollTop = 0; // For Chrome, Safari and Opera
-     document.documentElement.scrollTop = 0; // For IE and Firefox
- }
-
-
 
 
  $(window).on("load",function() {
@@ -102,9 +85,13 @@ $(document).ready(function() {
 
 
 
+ $(document).on('click', 'a', function(event){
+     event.preventDefault();
 
-
-
+     $('html, body').animate({
+         scrollTop: $( $.attr(this, 'href') ).offset().top
+     }, 500);
+ });
 
 
 
