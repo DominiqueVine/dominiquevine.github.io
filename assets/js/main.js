262b98f2fcb218e127b9fd4f77c5232dc5f9fa50
DominiqueVine <dcvin1@student.monash.edu>
2017-05-29 17:14:13 +1000

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,9 +31,9 @@ $(document).ready(function() {
 
        /* If the element is completely within bounds of the window, fade it in */
        if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-         if ($(this).css("opacity")==0) {$(this).fadeTo(700,1);}
+         if ($(this).css("opacity")==0) {$(this).fadeTo(400,1);}
        } else { //object goes out of view (scrolling up)
-         if ($(this).css("opacity")==1) {$(this).fadeTo(700,0);}
+         if ($(this).css("opacity")==1) {$(this).fadeTo(400,0);}
        }
      });
    }).scroll(); //invoke scroll-handler on page-load
@@ -148,6 +148,12 @@ $(document).ready(function() {
 
   $('.bruno').mouseleave(function() {
   $('.bruno__image').removeClass('showme');
+  });
+
+
+
+  $('.authorlist').click(function() {
+  $('.authorlist__image').toggleClass('showme');
   });
 
 
